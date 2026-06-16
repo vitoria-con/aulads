a5e0f34a088929500a0bb0170149e835f4c88466
vitoria.sousa.2008 <vitoria.sousa.2008@aluno.edu.pi.gov.br>
2026-06-16 19:10:08 +0000

    criei um objeto.js

--- multiplicaçao.test.js
+++ multiplicaçao.test.js
@@ -1,14 +1,16 @@
 const multiplicar = require ("./multiplicacao")
 test ("deve multiplicar dois numeros inteiros positivo",()=>{
+
     expect(multiplicar(3,2)).toBe(6)
 });
 
 test ("deve falhar, pois esta multiplicando uma string por e um valor numerico",()=>{
 espect (multiplicar -5).tothrow()});
 
-test ("deve multiplicar 2num.decimais"),()=>{
+test ("deve multiplicar 2Numero.decimais"),()=>{
     expect (multiplicar ( 2.5,3.0)).toBe(7.75);}
 
-test('deve somar dois números corretamente',()=>{
-        expect(sum(2, 3)).toBe(5);}
+test ('deve somar dois números corretamente'),()=>{
+        expect(somarNumero(2, 3)).toBe(5);}
+        modulo.exports = somarNumero;
 
---
+++ objeto.js
@@ -0,0 +1,13 @@
+const pessoa = {
+    nome:"vitoria",
+    idade:17,
+    peso:57,
+    altura:1.54,
+    cidade:"piripiri",
+}
+
+console.log (pessoa.nome);
+pessoa.idade = 18;
+console.log (pessoa.peso);
+pessoa.altura = 1.60;
+console.log (pessoa.cidade);
