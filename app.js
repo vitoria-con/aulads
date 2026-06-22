5baf255cf7f8b0e443b65cb61f84e2f00005a19c
vitoria.sousa.2008 <vitoria.sousa.2008@aluno.edu.pi.gov.br>
2026-06-22 15:10:24 +0000

    criei uma calculadora.js e app.js

---
+++ app.js
@@ -0,0 +1,38 @@
+const calculadora = require('./calculadora');
+
+// SOMA
+calculadora.numero1 = 15;
+calculadora.numero2 = 5;
+console.log("Soma:", calculadora.somar());
+
+// SUBTRAÇÃO
+calculadora.numero1 = 20;
+calculadora.numero2 = 8;
+console.log("Subtração:", calculadora.subtrair());
+
+// MULTIPLICAÇÃO
+calculadora.numero1 = 3;
+calculadora.numero2 = 4;
+console.log("Multiplicação:", calculadora.multiplicar());
+
+// DIVISÃO
+calculadora.numero1 = 20;
+calculadora.numero2 = 4;
+console.log("Divisão:", calculadora.dividir());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ calculadora.js
@@ -0,0 +1,47 @@
+const calculadora = {
+  numero1: 15,
+  numero2: 5,
+
+  somar() {
+    return this.numero1 + this.numero2;
+  },
+
+  subtrair() {
+    return this.numero1 - this.numero2;
+  },
+
+  multiplicar() {
+    return this.numero1 * this.numero2;
+  },
+
+  dividir() {
+    return this.numero1 / this.numero2;
+  }
+};
+
+module.exports = calculadora;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
